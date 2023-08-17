f3cd4d8f90258b0ec9f3dad7c1da93b8e4bddb8b
AlanHenriqueRF <alhenrangelfera@gmail.com>
2023-08-17 11:43:35 -0300

    fix: post timeline

--- src/routes/share.routes.js
+++ src/routes/share.routes.js
@@ -1,12 +1,11 @@
 import express from "express";
-import { LoginController,logOut,RegisterController } from "../controllers/authControllers.js";
 import { validateAuth } from "../middlewares/validateAuth.js";
 import{validateSchema }from "../middlewares/validateSchema.js"
 import { ShareSchema } from "../schemas/Sharpublish.Schema.js";
 import { SharePublish } from "../controllers/share.controller.js";
 const timelineRoute = express();
 
-timelineRoute.post('/timeline',validateSchema(ShareSchema),validateAuth,SharePublish);
+timelineRoute.post('/timeline',validateSchema(ShareSchema),validateAuth, SharePublish);
 
 
 export default timelineRoute
