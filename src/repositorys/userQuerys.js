5fd24fb57c2a1701013088d9f05a02ea3497635f
hp1heloisa <heloisapimentel2202@gmail.com>
2023-08-23 16:35:45 -0300

    fix searchUserDB

--- src/repositorys/userQuerys.js
+++ src/repositorys/userQuerys.js
@@ -2,9 +2,10 @@ import { db } from "../database/database.connection.js";
 
 export function searchUserDB(userId,text){
     return db.query(`
-        SELECT users.id, users.username, users.picture FROM users LEFT JOIN "followerRelationships" 
-        ON "followerRelationships"."followedUserId" = users.id WHERE users.username LIKE $1 ORDER BY 
-        CASE WHEN "followerRelationships"."followerId" = $2 THEN 0 ELSE 1 END, users.username;
+        SELECT "followerRelationships"."followerId" as seguidor, users.id, users.username, users.picture FROM users 
+        LEFT JOIN "followerRelationships" ON "followerRelationships"."followedUserId" = users.id WHERE 
+        users.username LIKE $1 ORDER BY CASE WHEN "followerRelationships"."followerId" = $2 THEN 0 ELSE 
+        1 END, users.username;
     `, [`%${text}%`, userId]);
 }
 
